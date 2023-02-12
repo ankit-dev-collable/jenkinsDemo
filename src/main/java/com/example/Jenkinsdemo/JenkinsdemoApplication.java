f1ea4ab96f2dd6f55485f855b210aceacfb06e7e
ankit-dev-collable <55022847+ankit-dev-collable@users.noreply.github.com>
2023-02-12 18:46:35 +0530

    Update JenkinsdemoApplication.java

--- src/main/java/com/example/Jenkinsdemo/JenkinsdemoApplication.java
+++ src/main/java/com/example/Jenkinsdemo/JenkinsdemoApplication.java
@@ -9,6 +9,9 @@ public class JenkinsdemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsdemoApplication.class, args);
 		System.out.println("hello");
+		System.out.println("jenkins demo for code commit");
+                System.out.println("hello");
+
 		
 
 	}
