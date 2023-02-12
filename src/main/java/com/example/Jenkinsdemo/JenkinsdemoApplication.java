75420f8699fcca592526b51ded4e333f92ee8f83
Ankit <cloudteche3@gmail.com>
2023-02-12 18:25:26 +0530

    code changes for jenkinsDemo

--- src/main/java/com/example/Jenkinsdemo/JenkinsdemoApplication.java
+++ src/main/java/com/example/Jenkinsdemo/JenkinsdemoApplication.java
@@ -9,9 +9,9 @@ public class JenkinsdemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsdemoApplication.class, args);
 		System.out.println("hello");
-		System.out.println("how");
 		System.out.println("hello");
-		System.out.println("how");
+		
+
 	}
 
 }
